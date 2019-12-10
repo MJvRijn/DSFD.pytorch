6cb28b3d57b30d70effdf4466dc184781e6da286
Matthew van Rijn <matthew.van.rijn@gmail.com>
2019-12-10 15:22:03 +0100

    Convert to python3

--- prepare_wider_data.py
+++ prepare_wider_data.py
@@ -1,15 +1,7 @@
-#-*- coding:utf-8 -*-
-
-from __future__ import division
-from __future__ import absolute_import
-from __future__ import print_function
-
-
 import os
 from data.config import cfg
-import cv2
 
-WIDER_ROOT = os.path.join(cfg.HOME, 'WIDER')
+WIDER_ROOT = cfg.FACE.WIDER_DIR
 train_list_file = os.path.join(WIDER_ROOT, 'wider_face_split',
                                'wider_face_train_bbx_gt.txt')
 val_list_file = os.path.join(WIDER_ROOT, 'wider_face_split',
@@ -46,7 +38,7 @@ def parse_wider_file(root, file):
     total_face = 0
     for k in face_count:
         face_ = []
-        for x in xrange(total_face, total_face + k):
+        for x in range(total_face, total_face + k):
             face_.append(face_loc[x])
         img_faces += [face_]
         total_face += k
@@ -56,7 +48,7 @@ def parse_wider_file(root, file):
 def wider_data_file():
     img_paths, bbox = parse_wider_file(WIDER_TRAIN, train_list_file)
     fw = open(cfg.FACE.TRAIN_FILE, 'w')
-    for index in xrange(len(img_paths)):
+    for index in range(len(img_paths)):
         path = img_paths[index]
         boxes = bbox[index]
         fw.write(path)
@@ -69,7 +61,7 @@ def wider_data_file():
 
     img_paths, bbox = parse_wider_file(WIDER_VAL, val_list_file)
     fw = open(cfg.FACE.VAL_FILE, 'w')
-    for index in xrange(len(img_paths)):
+    for index in range(len(img_paths)):
         path = img_paths[index]
         boxes = bbox[index]
         fw.write(path)
